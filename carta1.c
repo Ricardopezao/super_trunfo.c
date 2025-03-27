6d801f99ffe9c67319253c44c2e32dbcfa24f434
Ricardopezao <ricardoaguiarpezao@gmail.com>
2025-03-27 15:55:38 -0300

    Add files via upload

---
+++ carta1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main(){
+    
+    char estado,código, cidade;
+    int população;
+    float área;
+    float PIB;
+    int turisticos;
+
+
+    printf("digite estado\n");
+
+    scanf("%s" ,&estado);
+
+    printf("digite código\n");
+
+    scanf("%s" ,&código);
+
+    printf("digite cidade\n");
+
+    scanf("%s" ,&cidade);
+
+    printf("digite população\n");
+
+    scanf("%d" ,&população);
+
+    printf("digite área\n");
+
+    scanf("%f" ,&área);
+
+    printf("digite PIB");
+
+    scanf("%f" ,&PIB);
+
+    printf("digite turisticos\n");
+
+    scanf("%d" ,&turisticos); }
+
+    return 0;
