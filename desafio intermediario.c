6f931660800240979555b0583dbdc7d49eb46c81
Ricardopezao <ricardoaguiarpezao@gmail.com>
2025-04-08 16:42:58 -0300

    Add files via upload

---
+++ desafio intermediario.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+
+int main(){
+    
+    char Estado [2];
+    char Codigo [2];
+    char Cidade [6];
+    float Populacao;
+    float Area;
+    float PIB;
+    int Pontos turisticos;
+    float Densidade Demografica;
+    float PIB Per Capita;
+
+    printf("Digite o Estado:\n");
+    scanf("%s", &Estado);
+    
+    printf("Digite o Codigo:\n");
+    scanf("%s", &Codigo);
+
+    printf("Digite a Cidade:\n");
+    scanf("%s", &Cidade);
+
+    printf("Digite a Populacao:\n");
+    scanf("%f", &Populacao);
+
+    printf("Digite a Area:\n");
+    scanf("%f", &Area);
+
+    printf("Digite o PIB:\n");
+    scanf("%f", &PIB);
+
+    printf("Digite Pontos Turisticos:\n");
+    scanf("%d", &Pontos Turisticos);
+
+    printf("Digite Densidade Demografica:\n");
+    scanf("%f", &Densidade Demografica);
+
+    printf("Digite PIB Per Capita");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
