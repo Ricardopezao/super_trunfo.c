8575d386ef9680de044d6cfa7e648be4e06280e7
Ricardopezao <ricardoaguiarpezao@gmail.com>
2025-04-08 16:39:08 -0300

    Add files via upload

---
+++ desafio aventureiro.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+int main(){
+    char Estado [2];
+    char Codigo [3];
+    char Cidade [10];
+    float Populacao;
+    float Area;
+    float PIB;
+    int  PontosTuristicos;
+    float  DensidadePopulacional;
+    float PIBPerCapita;
+
+     printf("Digite o Estado:\n");
+    scanf("%s", &Estado);
+    
+    printf("Digite o Codigo:\n");
+    scanf("%s", &Codigo);
+
+    printf("Digite a Cidade\n");
+    scanf("%s", &Cidade);
+
+    printf("Digite a Populacao:\n");
+    scanf("%f", &Populacao);
+
+    printf("Digite a Area:\n");
+    scanf("%f",&Area);
+
+    printf("Digite o PIB:\n");
+    scanf("%f", &PIB);
+
+    printf("Dgite os Pontos Turisticos:\n");
+    scanf("%d", &PontosTuristicos);
+
+    printf("Digite a Densidade Populacional:\n");
+    scanf("%f", &DensidadePopulacional);
+    
+    printf("Digite o PIB Per Capita:\n");
+    scanf("%f", &PIBPerCapita);
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
